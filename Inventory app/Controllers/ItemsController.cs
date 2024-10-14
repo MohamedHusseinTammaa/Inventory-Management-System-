14e5714e51515859cc5237cd5933d27b5be34c5d
Mohamed Hussein <muhammed.tammaa@gmail.com>
2024-10-14 17:30:40 +0300

    intial commit

---
+++ Inventory app/Controllers/ItemsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Inventory_app.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View ();
+        }
+    }
+}
